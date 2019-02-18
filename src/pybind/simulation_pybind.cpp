f068704b36e59f56fb5165a8051e061b5c175560
craig-moir <craig.moir.01@gmail.com>
2019-02-18 12:43:46 +0000

    more work

--- src/pybind/simulation_pybind.cpp
+++ src/pybind/simulation_pybind.cpp
@@ -6,6 +6,11 @@
     // Simulation class
     py::class_<Simulation>(m, "Simulation")
 	.def(py::init<>())
+	.def_readonly("gravity", &Simulation::gravity)
+    	.def_readonly("frequency", &Simulation::frequency)
+    	.def_readonly("air_density", &Simulation::air_density)
+    	.def_readonly("c_air", &Simulation::sound_speed_air)
+	.def_readonly("c_particle", &Simulation::sound_speed_particle)
 	.def("add_transducer",
 	     &Simulation::add_transducer,
 	     "adds a transducer to the simulation",
@@ -46,12 +51,43 @@
 	     "focusses the transducers on a point in space",
 	     py::arg("point"))
 	.def("transducer", [](Simulation& sim, const size_t index) {
+		if (sim.transducers.size() < index+1) {
+		    std::cout << "Error: There aren't that many transducers!" << std::endl;
+		    throw;
+		}
 		return sim.transducers[index];
 	    })
-	.def_readonly("gravity", &Simulation::gravity)
-    	.def_readonly("frequency", &Simulation::frequency)
-    	.def_readonly("air_density", &Simulation::air_density)
-    	.def_readonly("c_air", &Simulation::sound_speed_air)
-	.def_readonly("c_particle", &Simulation::sound_speed_particle);
+	.def(py::pickle(
+			[](const Simulation& sim) { // __getstate__
+			    /* Return a tuple that fully encodes the state of the object */
+			    std::vector<Eigen::Vector3d> posv;
+			    std::vector<Eigen::Vector3d> dirv;
+			    std::vector<double> phiv;
+			    for (const Transducer t : sim.transducers) {
+				posv.push_back(t.pos);
+				dirv.push_back(t.director);
+				phiv.push_back(t.phi);
+			    }
+			    return py::make_tuple(posv, dirv, phiv);
+			},
+			[](py::tuple t) { // __setstate__
+			    if (t.size() != 3)
+				throw std::runtime_error("Invalid state!");
+
+			    /* Create a new C++ instance */
+			    Simulation sim;
+
+			    /* Assign additional transducer information */
+			    std::vector<Eigen::Vector3d> posv = t[0].cast<std::vector<Eigen::Vector3d> >();
+			    std::vector<Eigen::Vector3d> dirv = t[1].cast<std::vector<Eigen::Vector3d> >();
+			    std::vector<double> phiv = t[2].cast<std::vector<double> >();
+			    
+			    for (size_t i = 0; i < posv.size(); i++) {
+				sim.add_transducer(Transducer(posv[i], dirv[i], phiv[i]));
+			    }
+
+			    return sim;
+			}
+			));
     
 }
