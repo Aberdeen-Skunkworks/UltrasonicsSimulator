924289ed4c8454b5b5fe33a6e185cd6438ea802f
craig-moir <craig.moir.01@gmail.com>
2019-03-18 12:25:34 +0000

    updated example

--- examples/optimise_x_laplacian.py
+++ examples/optimise_x_laplacian.py
@@ -16,28 +16,21 @@ origin = [-0.05, 0.005, -0.05]
 tx, tz = 10, 10
 
 # creating a ultrasonics simulation
-try:
-    #this will fail
-    pickle_obj = pickle.load(open("opt_sim_x.pkl", "rb"))
-    sim = pickle_obj
-    print(sim)
-    print("I have loaded an old sim")
-except:
-    sim = us.Simulation()
+sim = us.Simulation()
 
-    for i in range(tx):
-        for j in range(tz):
+for i in range(tx):
+    for j in range(tz):
 
-            # transducer position
-            tpos = [origin[0] + L[0] * (i+0.5) / tx,
-                    0,
-                    origin[2] + L[2] * (j+0.5) / tz]
+        # transducer position
+        tpos = [origin[0] + L[0] * (i+0.5) / tx,
+                0,
+                origin[2] + L[2] * (j+0.5) / tz]
 
-            # creating a transducer
-            t = us.Transducer(tpos, [0, 1, 0])
+        # creating a transducer
+        t = us.Transducer(tpos, [0, 1, 0])
 
-            # adding transducer to simulation
-            sim.add_transducer(t)
+        # adding transducer to simulation
+        sim.add_transducer(t)
 
 # defining particle mass and diameter
 pm = 7.176591426e-7
@@ -50,31 +43,28 @@ print("optimisation point:", opt_point)
 
 # optimising
 print("Optimising transducer phases")
-#sim.optimise_Gorkov_laplacian([opt_point], 2e-6, pm, pd, True, False, False, 60, 100, 1e-0)
-#sim.optimise_Gorkov_laplacian([opt_point], 2e-6, pm, pd, True, False, False, 100, 100, 1e-0, "LN_BOBYQA")
-# sim.optimise_Gorkov_laplacian([opt_point], 2e-6, pm, pd, True, False, False, 100, 100, 1e-0, "LBFGS")
-sim.optimise_Gorkov_laplacian([opt_point], 2e-6, pm, pd, True, False, False, 100, 2000, 3e-5, "IPOPT")
+sim.optimise_Gorkov_laplacian([opt_point], 2e-6, pm, pd, True, False, False, 100, 200, 3e-5, "IPOPT")
+# There are a number of possible algorithm labels to use including: IPOPT,
+# LFBGS, LN_BOBYQA, and GN_ESCH
 
-# print("phi:", sim.transducer(0).phi)
 us.dump(sim, "optimised_phi_x_laplacian.vtu")
 
-# # defining the number of points for the field we are about to create
-# # N = [20, 20, 20]
-# N = [100, 100, 1]
-# L = [0.1, 0.1, 0.0]
-# origin = [-0.05, 0.005, 0.0]
+# defining the number of points for the field we are about to create
+N = [100, 100, 1]
+L = [0.1, 0.1, 0.0]
+origin = [-0.05, 0.005, 0.0]
 
-# # calculating Gorkov potential field
-# print("Calculating Gorkov potential field")
-# gorkov = sim.Gorkov_potential_field(N, L, origin, pm, pd)
+# calculating Gorkov potential field
+print("Calculating Gorkov potential field")
+gorkov = sim.Gorkov_potential_field(N, L, origin, pm, pd)
 
-# # outputing the Field to a vtr file viewable with Paraview
-# print("outputing Gorkov field to file")
-# us.dump(gorkov, "optimise_x_laplacian.vtr")
+# outputing the Field to a vtr file viewable with Paraview
+print("outputing Gorkov field to file")
+us.dump(gorkov, "optimise_x_laplacian.vtr")
 
-pickle_obj = {"sim", sim}
-pickle.dump(sim, open("opt_sim_x.pkl", "wb"))
-
+# recreating results of the nature communications paper buy
+# normalising the transducer phases by the phases focussed at the
+# optimisation point
 opt_phi = []
 for t in range(tx*tz):
     opt_phi.append(sim.transducer(t).phi)
@@ -84,12 +74,12 @@ sim.focus(opt_point)
 for t in range(tx*tz):
     opt_phi[t] -= sim.transducer(t).phi
     opt_phi[t] = opt_phi[t] % (2 * np.pi)
-    
-print(opt_phi)
 
 normalised_transducers = []
 for t, phi in enumerate(opt_phi):
     normalised_transducers.append(us.Transducer(sim.transducer(t).pos, sim.transducer(t).dir, phi))
 
+# outputing the normalised phases to a vtu file
+print("outputing normalised phases")
 us.dump(normalised_transducers, "normalised_transducers_x.vtu")
 
